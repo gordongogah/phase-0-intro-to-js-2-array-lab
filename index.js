aff1fdb60d9e3fc73fe354f23fd700f32c47ead3
gordongogah <gordongogah9@gmail.com>
2022-06-05 10:45:25 +0300

    commit tree 90dfe77f2317a79761cc7055dd9cb671c01a092c

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+//cats.push("Cat");
+//cats.unshift("Cat")
+//console.log(cats)
+function destructivelyAppendCat(name){
+   name = cats.push("Cat")
+};
+
+function destructivelyPrependCat(name){
+    name = cats.unshift("Cat")
+};
+function destructivelyRemoveLastCat(){
+    cats.pop()
+};
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+};
+function appendCat(name){
+    name = 'cat'
+}
+function removeLastCat(){
+    cats.pop('cat')
+};
+function removeFirsttCat(){
+    cats.shift()
+}
+//console.log(destructivelyAppendCat)
+//console.log(typeof(cats))
